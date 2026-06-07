1613079d5b044f92bbee21dc1f55bf42dd6cbb01
Sheersh01 <saxenasheersh1@gmail.com>
2026-06-07 20:17:31 +0530

    M.R.

--- src/Components/Circle.jsx
+++ src/Components/Circle.jsx
@@ -14,8 +14,8 @@ const Circle = () => {
 
   return (
     <div className='text-white w-32 h-fit circle'>
-       <div className="w-28 h-28 border-[1px]  border-white opacity-[0.5] rounded-full flex items-center justify-center absolute bottom-10 left-10  hover:scale-[0.8] hover:bg-white hover:text-black/[0.8] z-[2] transition-all">
-        <IoIosArrowRoundDown className="text-[5vw]"/>
+       <div className="w-16 h-16 md:w-28 md:h-28 border-[1px]  border-white opacity-[0.5] rounded-full flex items-center justify-center absolute bottom-5 left-5 md:bottom-10 md:left-10  hover:scale-[0.8] hover:bg-white hover:text-black/[0.8] z-[2] transition-all">
+        <IoIosArrowRoundDown className="text-3xl md:text-[5vw]"/>
         </div>
     </div>
   )
